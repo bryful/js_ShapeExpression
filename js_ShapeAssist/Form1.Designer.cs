83276b2ba0efd854dee288a489ee2019a2c24aaf
bryful <bryful@gmail.com>
2020-03-13 19:00:08 +0900

    assist

--- js_ShapeAssist/Form1.Designer.cs
+++ js_ShapeAssist/Form1.Designer.cs
@@ -31,9 +31,8 @@
             this.components = new System.ComponentModel.Container();
             this.ShapeAssist = new bryful_due.Ae_window();
             this.btnCreateShapeLayer = new bryful_due.Button_AE();
-            this.image_AE1 = new bryful_due.Image_AE();
-            this.ExportShape = new bryful_due.Button_AE();
-            ((System.ComponentModel.ISupportInitialize)(this.image_AE1)).BeginInit();
+            this.iconImag01 = new bryful_due.Image_AE();
+            ((System.ComponentModel.ISupportInitialize)(this.iconImag01)).BeginInit();
             this.SuspendLayout();
             // 
             // ShapeAssist
@@ -68,52 +67,35 @@
             this.btnCreateShapeLayer.Text = "create shapeLayer";
             this.btnCreateShapeLayer.UseVisualStyleBackColor = true;
             // 
-            // image_AE1
+            // iconImag01
             // 
-            this.image_AE1.AE_bounds = new System.Drawing.Rectangle(15, 99, 50, 50);
-            this.image_AE1.AE_defaultExt = ".png";
-            this.image_AE1.AE_imageFileName = "image_AE1_img.png";
-            this.image_AE1.AE_imageFilePath = "./";
-            this.image_AE1.AE_imageObjName = "image_AE1_img";
-            this.image_AE1.AE_isLocal = false;
-            this.image_AE1.AE_objName = "image_AE1";
-            this.image_AE1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.image_AE1.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.image_AE1.Location = new System.Drawing.Point(15, 99);
-            this.image_AE1.Name = "image_AE1";
-            this.image_AE1.Size = new System.Drawing.Size(50, 50);
-            this.image_AE1.TabIndex = 1;
-            this.image_AE1.TabStop = false;
-            // 
-            // ExportShape
-            // 
-            this.ExportShape.AE_bounds = new System.Drawing.Rectangle(15, 55, 320, 25);
-            this.ExportShape.AE_defaultElement = bryful_due.defaultElement.none;
-            this.ExportShape.AE_isLocal = true;
-            this.ExportShape.AE_objName = "btnExportShape";
-            this.ExportShape.AE_text = "export Shape";
-            this.ExportShape.AE_textObjName = "";
-            this.ExportShape.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.ExportShape.Location = new System.Drawing.Point(15, 55);
-            this.ExportShape.Name = "ExportShape";
-            this.ExportShape.Size = new System.Drawing.Size(320, 25);
-            this.ExportShape.TabIndex = 2;
-            this.ExportShape.Text = "export Shape";
-            this.ExportShape.UseVisualStyleBackColor = true;
+            this.iconImag01.AE_bounds = new System.Drawing.Rectangle(15, 59, 50, 50);
+            this.iconImag01.AE_defaultExt = ".png";
+            this.iconImag01.AE_imageFileName = "iconImag01.png";
+            this.iconImag01.AE_imageFilePath = "./";
+            this.iconImag01.AE_imageObjName = "iconImag01";
+            this.iconImag01.AE_isLocal = true;
+            this.iconImag01.AE_objName = "iconImag01";
+            this.iconImag01.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.iconImag01.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.iconImag01.Location = new System.Drawing.Point(15, 59);
+            this.iconImag01.Name = "iconImag01";
+            this.iconImag01.Size = new System.Drawing.Size(50, 50);
+            this.iconImag01.TabIndex = 1;
+            this.iconImag01.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(345, 361);
-            this.Controls.Add(this.ExportShape);
-            this.Controls.Add(this.image_AE1);
+            this.Controls.Add(this.iconImag01);
             this.Controls.Add(this.btnCreateShapeLayer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ShapeAssist";
-            ((System.ComponentModel.ISupportInitialize)(this.image_AE1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconImag01)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -122,8 +104,7 @@
 
         private bryful_due.Ae_window ShapeAssist;
         private bryful_due.Button_AE btnCreateShapeLayer;
-        private bryful_due.Button_AE ExportShape;
-        private bryful_due.Image_AE image_AE1;
+        private bryful_due.Image_AE iconImag01;
     }
 }
 
--- js_ShapeAssist/ScriptCode/ShapeAssist.jsx
+++ js_ShapeAssist/ScriptCode/ShapeAssist.jsx
@@ -1,15 +1,46 @@
 ﻿(function(me){
+	var iconsFiles = []; //アイコンファイルのパス配列	var iconsFunc = [];//プリセット/jsxファイルのパス配列	var icons = []; // imageの配列		//----------------------------------
+	//prototype登録
+	String.prototype.trim = function(){
+		if (this=="" ) return ""
+		else return this.replace(/[\r\n]+$|^\s+|\s+$/g, "");
+	}
+	String.prototype.getParent = function(){
+		var r=this;var i=this.lastIndexOf("/");if(i>=0) r=this.substring(0,i);
+		return r;
+	}
+	//ファイル名のみ取り出す（拡張子付き）
+	String.prototype.getName = function(){
+		var r=this;var i=this.lastIndexOf("/");if(i>=0) r=this.substring(i+1);
+		return r;
+	}
+	//拡張子のみを取り出す。
+	String.prototype.getExt = function(){
+		var r="";var i=this.lastIndexOf(".");if (i>=0) r=this.substring(i);
+		return r;
+	}
+	//指定した書拡張子に変更（dotを必ず入れること）空文字を入れれば拡張子の消去。
+	String.prototype.changeExt=function(s){
+		var i=this.lastIndexOf(".");
+		if(i>=0){return this.substring(0,i)+s;}else{return this; }
+	}
+	//文字の置換。（全ての一致した部分を置換）
+	String.prototype.replaceAll=function(s,d){ return this.split(s).join(d);}
 
-	//-------------------------------------------------------------------------
-	var winObj = ( me instanceof Panel) ? me : new Window("palette", "ShapeAssist", [ 779,  320,  779+ 345,  320+ 361]  ,{resizeable:true, maximizeButton:true, minimizeButton:true});
-	//-------------------------------------------------------------------------
+	
+	//----------------------------------
+	var scriptName = File.decode($.fileName.getName().changeExt(""));
+	var targetFolder = new Folder ( $.fileName.getParent() + "/(" +$.fileName.getName().changeExt("")+ ")");	// ************************************************************************
+	var iconLoad = function()	{		var ret = false;		iconsFiles = []; //アイコンファイルのパス配列		iconsFunc = [];//プリセットファイルのパス配列		if (targetFolder.exists == false) {			alert("フォルダがないです");			return ret;		}		var pngFiles = f.getFiles("*.png");		if (pngFiles.length <=0) {			alert("pngファイルがないです");			return ret;		}		// ffx/jsxを探す		for ( var i = 0; i<pngFiles.length; i++){			var jsx = new File( pngFiles[i].fullName.changeExt(".jsx"));			var ffx = new File( pngFiles[i].fullName.changeExt(".ffx"));			if (jsx.exists == true) {				iconsFiles.push(pngFiles[i]);				ffx.isFFX = false;				ffx.isJSX = true;				iconsFunc.push(ffx);			}else{				if (ffx.exists == true) {					iconsFiles.push(pngFiles[i]);					ffx.isFFX = true;					ffx.isJSX = false;					iconsFunc.push(ffx);				}			}		}		ret = (iconsFiles.length > 0);		if ( ret==false )		{			alert("jsx/ffxファイルがないです");		}		return ret;
+	}	iconLoad(targetFolder);	// ************************************************************************	var initIcons = function()	{
+		if (icons.length ==0) return;				for ( var i= icons.length-1; i>=0; i--)		{			icons[i].visible = false;			delete icons[i];			icons[i] = null;		}		icons = [];	}
+	// ************************************************************************
+	var winObj = ( me instanceof Panel) ? me : new Window("palette", "ShapeAssist", [ 0,  0, 345,  360]  ,{resizeable:true, maximizeButton:true, minimizeButton:true});
+	// ************************************************************************
 	var btnCreateShapeLayer = winObj.add("button", [  15,   15,   15+ 320,   15+  25], "create shapeLayer" );
-	btnCreateShapeLayer.graphics.font = ScriptUI.newFont("Tahoma",ScriptUI.FontStyle.REGULAR, 11);
-	var btnExportShape = winObj.add("button", [  15,   55,   15+ 320,   55+  25], "export Shape" );
-	btnExportShape.graphics.font = ScriptUI.newFont("Tahoma",ScriptUI.FontStyle.REGULAR, 11);
-
-	this.image_AE1 = winObj.add("image", [  15,   99,   15+  50,   99+  50] /*, image_AE1_img*/ );
-
+	var iconImag01 = winObj.add("image", [  15,   59,   15+  50,   59+  50] /*, iconImag01*/ );
+	// ************************************************************************
+	var createIcons = function()	{		initIcons();		if (iconsFiles.length<=0) return;		for ( var i=0; i<iconsFiles.length; i++)		{		}	}
 	// ************************************************************************
 	var getActiveComp = function()
 	{
@@ -102,29 +133,11 @@
 	// ************************************************************************
 	btnCreateShapeLayer.onClick = createShapeLayer;
 	// ************************************************************************
-	var exportShape = function()
-	{
-		var str = "";
-		// ******************************
-		var exportShapeSubProperty = function(p,parentPG)
-		{
-		}
-		// ******************************
-		var exportShapeSub = function(prop,paretPG)
-		{
-			if (prop instanceof Property)
-			{
-				return;
-			}
-			
-		}
-		// ******************************
-	
-		var pg = getPropertyGroup();
-		alert(pg.name);
-	}
-	btnExportShape.onClick = exportShape;
-	//-------------------------------------------------------------------------
+	var resizeWin = function()	{		var b = winObj.bounds;
+		var w = b[2] - b[0];
+		var h = b[3] - b[1];				var b2 = btnCreateShapeLayer.bounds;		b2[0] = 15;		b2[2] = b2[0] + (w - 30);		btnCreateShapeLayer.bounds = b2;		
+	}	resizeWin();
+	winObj.onResize = resizeWin;	//-------------------------------------------------------------------------
 	if ( ( me instanceof Panel) == false){
 		winObj.center(); 
 		winObj.show();
