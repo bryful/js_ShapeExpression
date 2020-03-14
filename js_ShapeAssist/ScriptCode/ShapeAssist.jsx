48d14f37c4de0cb684eaa9b1d36905b64895b3b8
古橋 宏 <bryful@gmail.com>
2020-03-15 00:08:09 +0900

    script

--- js_ShapeAssist/ScriptCode/ShapeAssist.jsx
+++ js_ShapeAssist/ScriptCode/ShapeAssist.jsx
@@ -1,8 +1,13 @@
 ﻿(function(me){
 
-	var iconsFiles = []; //アイコンファイルのパス配列
-	var iconsFunc = [];//プリセット/jsxファイルのパス配列
-	var icons = []; // imageの配列
+	var imageWidth = 32;
+	var imageHeight = 32;
+	
+	var imageFiles = []; //アイコンファイルのパス配列
+	var imageFuncs = [];//プリセット/jsxファイルのパス配列
+
+	var imageCtrls = []; // imageの配列
+	
 	
 	//----------------------------------
 	//prototype登録
@@ -36,76 +41,132 @@
 	//----------------------------------
 	var scriptName = File.decode($.fileName.getName().changeExt(""));
 	var targetFolder = new Folder ( $.fileName.getParent() + "/(" +$.fileName.getName().changeExt("")+ ")");
-
-	// ************************************************************************
-	var iconLoad = function()
+	// ********************************************************************************
+	var getActiveComp = function()
 	{
-		var ret = false;
-		iconsFiles = []; //アイコンファイルのパス配列
-		iconsFunc = [];//プリセットファイルのパス配列
-		if (targetFolder.exists == false) {
-			alert("フォルダがないです");
-			return ret;
-		}
-		var pngFiles = f.getFiles("*.png");
-		if (pngFiles.length <=0) {
-			alert("pngファイルがないです");
-			return ret;
-		}
-		// ffx/jsxを探す
-		for ( var i = 0; i<pngFiles.length; i++){
-			var jsx = new File( pngFiles[i].fullName.changeExt(".jsx"));
-			var ffx = new File( pngFiles[i].fullName.changeExt(".ffx"));
-			if (jsx.exists == true) {
-				iconsFiles.push(pngFiles[i]);
-				ffx.isFFX = false;
-				ffx.isJSX = true;
-				iconsFunc.push(ffx);
-			}else{
-				if (ffx.exists == true) {
-					iconsFiles.push(pngFiles[i]);
-					ffx.isFFX = true;
-					ffx.isJSX = false;
-					iconsFunc.push(ffx);
-				}
-			}
-		}
-		ret = (iconsFiles.length > 0);
-		if ( ret==false )
+		var ret = null;
+		ret = app.project.activeItem;
+		
+		if ( (ret instanceof CompItem)===false)
 		{
-			alert("jsx/ffxファイルがないです");
+			ret = null;
+			alert("コンポをアクティブにしてください！");
 		}
 		return ret;
 	}
-	iconLoad(targetFolder);
-	// ************************************************************************
-	var initIcons = function()
+	// ********************************************************************************
+	var getActiveLayer = function(cmp)
 	{
-		if (icons.length ==0) return;
-		
-		for ( var i= icons.length-1; i>=0; i--)
-		{
-			icons[i].visible = false;
-			delete icons[i];
-			icons[i] = null;
+		var ret = null;
+		if ( (cmp instanceof CompItem)==false) return ret;
+		var sl = cmp.selectedLayers;
+		if (sl.length<=0) {
+			alert("	レイヤが選択されていません");
+			return ret;
 		}
-		icons = [];
+		ret = sl[0];
+		return ret;
+	}
+	// ********************************************************************************
+	var getActivePropertyGroup = function(cmp)
+	{
+		var ret = null;
+		if ( (cmp instanceof CompItem)==false) return ret;
+		var sp = cmp.selectedProperties;
+		if (sp.length<=0) {
+			alert("	プロパティが選択されていません");
+			return ret;
+		}
+		ret = sp[0];
+		return ret;
+	}
+	// ************************************************************************
+	var folderScan = function(f)
+	{
+		var ret = false;
+		if (f.exists == false) {
+			alert(f.name + " : フォルダがないです");
+			return ret;
+		}
+		imageFiles = []; //アイコンファイルのパス配列
+		imageFuncs = [];
+
+		var pngFiles = f.getFiles("*.png");
+			
+		for ( var i=0; i<pngFiles.length; i++)
+		{
+			var ffx = new File( pngFiles[i].fullName.changeExt(".ffx"));
+			if (ffx.exists == true) {
+				imageFiles.push(pngFiles[i]);
+				imageFuncs.push(ffx);
+			}	
+		}
+		ret = (imageFiles.length > 0);
+		if ( ret==false )
+		{
+			alert("ffxファイルがないです");
+		}
+		return ret;
+	}
+	folderScan(targetFolder);
+	
+	// ************************************************************************
+	var initImageCtrls = function()
+	{
+		if (imageCtrls.length ==0) return;
+		
+		for ( var i= imageCtrls.length-1; i>=0; i--)
+		{
+			imageCtrls[i].visible = false;
+			delete imageCtrls[i];
+			imageCtrls[i] = null;
+		}
+		imageCtrls = [];
 	}
 	// ************************************************************************
 	var winObj = ( me instanceof Panel) ? me : new Window("palette", "ShapeAssist", [ 0,  0, 345,  360]  ,{resizeable:true, maximizeButton:true, minimizeButton:true});
 	// ************************************************************************
 	var btnCreateShapeLayer = winObj.add("button", [  15,   15,   15+ 320,   15+  25], "create shapeLayer" );
-	var iconImag01 = winObj.add("image", [  15,   59,   15+  50,   59+  50] /*, iconImag01*/ );
+	// ************************************************************************
+	var applyFunc = function()
+	{
+		var ac = getActiveComp();
+		var ap = getActivePropertyGroup(ac);
+		var lyr = getActiveLayer(ac);
+		if ( (lyr instanceof ShapeLayer) == false) {
+			alert("シェイプレイヤを選んでください");
+			return;
+		}
+		if ( ap == null) {
+			alert("何か選んでください");
+			return;
+		}
+		var f = this.funcFile;
+		if ( (ap.matchName =="ADBE Root Vectors Group")||(ap.matchName =="ADBE Vector Group")){
+			lyr.applyPreset(f);
+			
+		}else{
+			alert("	シェイプグループが選択されていません");
+		}
+	}	
 
 	// ************************************************************************
-	var createIcons = function()
+	var createImageCtrls = function()
 	{
-		initIcons();
-		if (iconsFiles.length<=0) return;
-		for ( var i=0; i<iconsFiles.length; i++)
+		initImageCtrls();
+		if (imageFiles.length<=0) return;
+		var x = 15;
+		for ( var i=0; i<imageFiles.length; i++)
 		{
+
+			imageCtrls.push(winObj.add("iconbutton", [  x,   50,   x +  imageWidth,   50 +  imageHeight] , imageFiles[i]));
+			imageCtrls[i].funcFile = imageFuncs[i];
+			imageCtrls[i].onClick = applyFunc;
+			imageCtrls[i].visible = true;
+			x += imageWidth;
 		}
 	}
+	createImageCtrls();
 	// ************************************************************************
 	var getActiveComp = function()
 	{
@@ -209,6 +270,27 @@
 		b2[2] = b2[0] + (w - 30);
 		btnCreateShapeLayer.bounds = b2;
 		
+		if (imageCtrls.length>0) {
+			var xmax = w - imageWidth/2;
+			
+			var xm = b2[0];
+			var ym = b2[3] + xm;
+			var xpos = xm;
+			var ypos = ym;
+			for ( var i = 0; i<imageCtrls.length; i++){
+				ib = imageCtrls[i].bounds;
+				ib[0] = xpos;
+				ib[1] = ypos;
+				ib[2] = xpos + imageWidth;
+				ib[3] = ypos + imageHeight;
+				imageCtrls[i].bounds = ib;
+				xpos += imageWidth;
+				if (xpos>=xmax){
+					xpos = xm;
+					ypos += imageHeight;
+				}
+			}
+		}
 	}
 	resizeWin();
 	winObj.onResize = resizeWin;
